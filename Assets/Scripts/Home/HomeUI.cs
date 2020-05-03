33398e95be5b0562a6ad34afc0ddaafab4ee86ba
kylon <kylonux@gmail.com>
2020-05-03 18:57:38 +0200

    menu

---
+++ Assets/Scripts/Home/HomeUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Apocalypse {
+    public class HomeUI : MonoBehaviour {
+        private Button _startBtn;
+
+        private void Awake() {
+            _startBtn = GetComponent<Button>();
+        }
+
+        void Start () {
+            _startBtn.onClick.AddListener(startGame);
+        }
+
+        private void startGame() {
+            Debug.Log("lll");
+            SceneManager.LoadScene("ShopScene");
+        }
+    }
+}
